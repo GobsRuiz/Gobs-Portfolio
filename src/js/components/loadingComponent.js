9d0727ec5fbe4e003576b3b0efd6f1da29be4a19
GobsRuiz <gbibisruiz@hotmail.com>
2025-03-20 16:17:45 -0300

    .

--- src/js/components/loadingComponent.js
+++ src/js/components/loadingComponent.js
@@ -2,8 +2,11 @@ function loadingComponent()
 {
     const element = document.querySelector(".loadingComponent") ;
 
+    document.body.style.overflow = 'hidden'
+
     setTimeout(() => {
         element.style.display = 'none';
+        document.body.style.overflow = 'initial'
     }, 2150);
 }
 loadingComponent()
